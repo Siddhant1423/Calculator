c29111c2804ce27038916af284c47eff159aaadc
Siddhant1423 <130544314+Siddhant1423@users.noreply.github.com>
2025-01-01 12:04:23 +0530

    Made changes to JS file and HTML/CSS.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const add = (a, b) => {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 const subtract = (a, b) => {
     return a - b;
@@ -24,10 +24,20 @@ const operate = (a, b, operand) => {
     }
 }
 
-const getOperand = () =>{
-    
+let isOperatorClicked = false;
+const handleClick = () => {
+    if(isOperatorClicked == true){
+        btnOperand.forEach(item => item.disabled = true)
+    }else{
+        btnOperand.forEach(item => item.disabled = false)
+    }
 }
+
+let result = 0;
+
 const displayInput = document.querySelector("#display-one")
+const displayInputTwo = document.querySelector("#display-two")
+
 
 const btn = document.querySelectorAll('.btn-number');
 btn.forEach((button)=>{
@@ -39,11 +49,58 @@ btn.forEach((button)=>{
 
 });
 
+// OPERANDS ==========================
+
 const btnOperand = document.querySelectorAll(".operator");
 btnOperand.forEach((button)=>{
     button.addEventListener('click',() => {
         let op = button.getAttribute('data-value')
-        displayInput.textContent += " "+op+" ";
+        displayInput.textContent += ` ${op} `;
         console.log(op)
-    })
+        isOperatorClicked = true;
+        handleClick();
+
+
+    },)
+})
+
+// EQUALS BUTTON =====================
+
+const equalbtn = document.querySelector('#equals-btn');
+equalbtn.addEventListener('click',()=>{
+
+    if(result == 0 ){
+
+        let inputNumber = displayInput.textContent;
+        let splitValues = inputNumber.split(" ");
+        let a = splitValues[0];
+        let b = splitValues[2]
+        let opr = splitValues[1]
+        
+        result = operate(a,b,opr)
+        displayInputTwo.textContent = result.toFixed(3);
+        isOperatorClicked = false;
+        handleClick();
+        console.log(inputNumber);
+    }
+    else{
+        
+    }
+
+    
+});
+
+// RESET BUTTON =======================
+
+const resetbtn = document.querySelector('#allclear-btn');
+resetbtn.addEventListener('click',() => {
+    displayInput.textContent = "";
+    displayInputTwo.textContent = "";
+})
+
+// BACKSPACE BUTTON ===================
+
+const bkspace = document.querySelector('#cancel-btn');
+bkspace.addEventListener('click',()=>{
+    displayInput.textContent = displayInput.textContent.slice(0,-1)
 })
